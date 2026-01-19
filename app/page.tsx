94dc1e2884cfeab0b338456f842a7b4263c65eb1
Novel272 <mohammedalmustafak@gmail.com>
2026-01-19 22:51:03 +0400

    load real events again

--- app/page.tsx
+++ app/page.tsx
@@ -9,8 +9,6 @@ const baseUrl = process.env.NEXT_PUBLIC_BASE_URL;
 
 
 const Page = async () => {
-  'use cache';
-  cacheLife("hours");
 
   if (!baseUrl) {
     throw new Error('NEXT_PUBLIC_BASE_URL is not defined');
